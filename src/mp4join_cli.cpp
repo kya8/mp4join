dfe4fcd3c0d65cd083dcb09d86149a72a5d2ebc0
kya8 <46643054+kya8@users.noreply.github.com>
2025-04-26 00:15:55 +0800

    cli: Minor adjustments

--- src/mp4join_cli.cpp
+++ src/mp4join_cli.cpp
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
                 if (++i < argc) output = argv[i];
                 else err_flag = 1;
             }
-            else if (!std::strcmp(argv[i], "-v")) {
+            else if (!std::strcmp(argv[i], "-V")) {
                 print_version = true;
                 break;
             }
@@ -32,11 +32,13 @@ int main(int argc, char** argv)
 
         if (print_version) {
             using namespace mp4join::version;
-            std::printf("Version %s, %s\n", COMMIT_HASH, COMMIT_DATE);
+            std::printf("Version %s, %s, %s. Built for %s %s, using %s %s\n",
+                COMMIT_HASH, COMMIT_DATE, BUILD_TYPE,
+                TARGET_OS, TARGET_ARCH, COMPILER_NAME, COMPILER_VERSION);
             return 0;
         }
         if (err_flag || !output || inputs.size() < 2) {
-            std::puts("Usage: mp4join <file_1> <file_2> [...] <-o output_file> [-v]");
+            std::puts("Usage: mp4join <file_1> <file_2> [...] <-o output_file> [-V]");
             return 1;
         }
     }
@@ -48,24 +50,27 @@ int main(int argc, char** argv)
     int prog_prev = -1;
     
     const auto prog_cb = [&] (int prog_) {
-        prog.store(prog_);
+        prog.store(prog_, std::memory_order_release);
     };
     std::thread worker {
         [&] {
-            ret = mp4_join((int)inputs.size(), inputs.data(), output, prog_cb);
-            done.store(true);
+            ret = mp4_join(static_cast<int>(inputs.size()), inputs.data(), output, prog_cb);
+            done.store(true, std::memory_order_release);
         }
     };
 
-    for (int cnt = 0; !done.load(); cnt += (cnt < 10), std::this_thread::sleep_for(std::chrono::milliseconds(cnt <= 3 ? 1 : 100))) {
-        const auto prog_new = prog.load();
+    static constexpr int busy_spin_cnt = 5;
+    static constexpr int busy_spin_interval = 1;
+    static constexpr int relaxed_spin_interval = 100;
+    for (int cnt = 0; !done.load(std::memory_order_acquire); cnt += (cnt < busy_spin_cnt), std::this_thread::sleep_for(std::chrono::milliseconds(cnt < busy_spin_cnt ? busy_spin_interval : relaxed_spin_interval))) {
+        const auto prog_new = prog.load(std::memory_order_acquire);
         if (prog_new > prog_prev) {
-            prog_prev = prog_new;
             static char line_buf[32];
             // print the whole string at once to avoid cursor flickering observed on MinGW
-            std::snprintf(line_buf, 32, "\rProgress: %d%%", prog_prev);
+            std::snprintf(line_buf, 32, "\rProgress: %d%%", prog_new);
             std::fputs(line_buf, stdout);
             std::fflush(stdout);
+            prog_prev = prog_new;
         }
     }
 
