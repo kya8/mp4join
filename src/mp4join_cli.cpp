349e85d66426c4894457ebaf6a7f4f7d127ea668
tao <>
2025-05-03 11:42:36 +0800

    cli: Check for existing output; Auto output filename

--- src/mp4join_cli.cpp
+++ src/mp4join_cli.cpp
@@ -5,29 +5,31 @@
 #include <vector>
 #include <thread>
 #include <atomic>
+#include <filesystem>
 
+namespace fs = std::filesystem;
 
 int main(int argc, char** argv)
 {
     std::vector<const char*> inputs;
-    const char* output = nullptr;
+    fs::path output;
+    bool always_overwrite = false;
 
     {
         bool print_version = false;
-        bool err_flag = 0;
-        for (int i = 1; i < argc; ++i) {
+        int err_flag = 0;
+        for (int i = 1; i < argc && !err_flag; ++i) {
             if (!std::strcmp(argv[i], "-o")) {
                 if (++i < argc) output = argv[i];
                 else err_flag = 1;
-            }
-            else if (!std::strcmp(argv[i], "-V")) {
+            } else if (!std::strcmp(argv[i], "-f")) {
+                always_overwrite = true;
+            } else if (!std::strcmp(argv[i], "-V")) {
                 print_version = true;
                 break;
+            } else {
+                inputs.push_back(argv[i]);
             }
-            else {
-                inputs.emplace_back(argv[i]);
-            }
-            if (err_flag) break;
         }
 
         if (print_version) {
@@ -37,9 +39,25 @@ int main(int argc, char** argv)
                 TARGET_OS, TARGET_ARCH, COMPILER_NAME, COMPILER_VERSION);
             return 0;
         }
-        if (err_flag || !output || inputs.size() < 2) {
-            std::puts("Usage: mp4join <file_1> <file_2> [...] <-o output_file> [-V]");
+        if (err_flag || inputs.size() < 2) {
+            std::puts("Usage: mp4join <file_1> <file_2> [...] [-o output_file] [-f] [-V]");
             return 1;
+        }
+    }
+
+    if (output.empty()) {
+        output = inputs[0];
+        output.replace_filename(output.stem().concat("_joined") += output.extension());
+    }
+
+    const auto output_str = output.string();
+
+    if (!always_overwrite && fs::is_regular_file(output)) {
+        std::printf("Output file %s already exists. Okay to overwrite? [y/N] ", output_str.c_str());
+        const char answer = std::getchar();
+        if (answer != 'y' && answer != 'Y') {
+            std::puts("Aborting.");
+            return 0;
         }
     }
 
@@ -48,13 +66,13 @@ int main(int argc, char** argv)
     std::atomic<bool> done = false;
     std::atomic<int> prog = -1;
     int prog_prev = -1;
-    
+
     const auto prog_cb = [&] (int prog_) {
         prog.store(prog_, std::memory_order_release);
     };
     std::thread worker {
         [&] {
-            ret = mp4_join(static_cast<int>(inputs.size()), inputs.data(), output, prog_cb);
+            ret = mp4_join(static_cast<int>(inputs.size()), inputs.data(), output_str.c_str(), prog_cb);
             done.store(true, std::memory_order_release);
         }
     };
@@ -79,7 +97,7 @@ int main(int argc, char** argv)
     std::putchar('\r');
     switch (ret) {
     case(JoinResult::Success):
-        std::printf("MP4 join done: %s\n", output);
+        std::printf("MP4 join done: %s\n", output_str.c_str());
         break;
     case(JoinResult::InvalidInput):
         std::puts("MP4 join error: Invalid input file.");
