396f1ede7d3f5302f8be8ddc3f35a2669e85edfe
kya <46643054+kya8@users.noreply.github.com>
2025-08-13 17:46:59 +0800

    cli: Check for existing output file

--- src/mp4join_cli.cpp
+++ src/mp4join_cli.cpp
@@ -5,11 +5,20 @@
 #include <vector>
 #include <thread>
 #include <atomic>
+#include <filesystem>
 #include "is_tty.hpp"
+
+namespace fs = std::filesystem;
 
 namespace {
 
 const bool stdout_is_tty = is_tty(stdout);
+
+bool get_yn() noexcept
+{
+    const auto ans = std::getchar();
+    return ans == 'y' || ans == 'Y';
+}
 
 } // namespace
 
@@ -17,34 +26,54 @@ int main(int argc, char** argv)
 {
     std::vector<const char*> inputs;
     const char* output = nullptr;
+    bool force_overwrite = false;
 
     {
         bool print_version = false;
-        bool err_flag = 0;
-        for (int i = 1; i < argc; ++i) {
+        bool bad_arg = false;
+        for (int i = 1; i < argc && !bad_arg; ++i) {
             if (!std::strcmp(argv[i], "-o")) {
                 if (++i < argc) output = argv[i];
-                else err_flag = 1;
-            }
-            else if (!std::strcmp(argv[i], "-V")) {
+                else bad_arg = true;
+            } else if (!std::strcmp(argv[i], "-V")) {
                 print_version = true;
                 break;
-            }
-            else {
+            } else if (!std::strcmp(argv[i], "-f")) {
+                force_overwrite = true;
+            } else {
                 inputs.emplace_back(argv[i]);
             }
-            if (err_flag) break;
         }
 
         if (print_version) {
             using namespace mp4join::version;
-            std::printf("Version %s, %s, %s. Built for %s %s, using %s %s\n",
-                COMMIT_HASH, COMMIT_DATE, BUILD_TYPE,
+            std::printf("Version %s, %s. Built type %s, %s %s, using %s %s\n",
+                GIT_DESC, COMMIT_DATE, BUILD_TYPE,
                 TARGET_OS, TARGET_ARCH, COMPILER_NAME, COMPILER_VERSION);
             return 0;
         }
-        if (err_flag || !output || inputs.size() < 2) {
-            std::puts("Usage: mp4join <file_1> <file_2> [...] <-o output_file> [-V]");
+        if (bad_arg || !output || inputs.size() < 2) {
+            std::fputs(
+                "Usage: mp4join <file_1> <file_2> [...] <-o output_file> [-V]\n"
+                "\nmp4join: Utility for joining consecutive MP4 files.\n"
+                "\nOptions:\n"
+                " -o output   Specify output file.\n"
+                " -f          Always overwrite existing output file.\n"
+                " -V          Display version information.\n"
+                , stdout);
+            return 1;
+        }
+    }
+
+    if (!force_overwrite && fs::is_regular_file(fs::path(output))) {
+        if (stdout_is_tty) {
+            std::printf("Output file %s already exists. Overwrite? [y/N] ", output);
+            if (!get_yn()) {
+                std::fputs("Aborting.\n", stdout);
+                return 0;
+            }
+        } else {
+            std::fputs("Refusing to overwrite existing file.\n", stderr);
             return 1;
         }
     }
